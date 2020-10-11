f5c15716e97a68f6085c6816d12984046dd6d2d8
Willian Rosa <willian.rosa@bit55.com.br>
2020-10-11 10:26:44 -0300

    module exceptions unit tests

--- tests/lamper/test_exceptions.py
+++ tests/lamper/test_exceptions.py
@@ -1,33 +1,48 @@
 from collections import OrderedDict
 
-from lamper.exceptions import HttpBaseException, CustomException
+from lamper import exceptions
 from lamper.https import HttpResponse, HttpStatusCode
 
 
 def test_get_response():
-    response = HttpBaseException(HttpResponse.success()).get_response()
+    response = exceptions.HttpBaseException(HttpResponse.success()).get_response()
     assert response == {'statusCode': 200, 'body': None}
 
 
 def test_get_response_with_body():
-    response = HttpBaseException(HttpResponse.success(), body={'message': 'ok'}).get_response()
+    response = exceptions.HttpBaseException(HttpResponse.success(), body={'message': 'ok'}).get_response()
     assert response == {'statusCode': 200, 'body': '{"message": "ok"}'}
 
 
 def test_get_response_bad_request():
-    response = HttpBaseException(HttpResponse.bad_request()).get_response()
+    response = exceptions.HttpBaseException(HttpResponse.bad_request()).get_response()
     assert response.get('statusCode') == 400
 
 
 def test_get_response_unauthorized():
-    response = HttpBaseException(HttpResponse.unauthorized()).get_response()
+    response = exceptions.HttpBaseException(HttpResponse.unauthorized()).get_response()
     assert response.get('statusCode') == 401
 
 def test_get_response_error():
-    response = HttpBaseException(HttpResponse.error()).get_response()
+    response = exceptions.HttpBaseException(HttpResponse.error()).get_response()
     assert response.get('statusCode') == 500
 
 def test_custom_exception():
-    response = CustomException(HttpStatusCode.ERROR, None).get_response()
+    response = exceptions.CustomException(HttpStatusCode.ERROR, None).get_response()
     assert response.get('statusCode') == 500
 
+def test_badrequest_exception():
+    response = exceptions.BadRequestException().get_response()
+    assert response.get('statusCode') == 400
+
+def test_unauthorized_exception():
+    response = exceptions.UnauthorizedException().get_response()
+    assert response.get('statusCode') == 401
+
+def test_forbidden_exception():
+    response = exceptions.ForbiddenException().get_response()
+    assert response.get('statusCode') == 403
+
+def test_error_exception():
+    response = exceptions.ErrorException().get_response()
+    assert response.get('statusCode') == 500
